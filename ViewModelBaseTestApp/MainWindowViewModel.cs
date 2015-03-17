63ea61a8b27a7c30cf46293d50ecad7aa5c1674d
MakMokMak <mak@makcraft.com>
2015-03-17 16:20:13 +0900

    ViewModelBaseTestAPP に別スレッド(タイマー)からのコマンド実行可能条件の変更のテストを追加

--- ViewModelBaseTestApp/MainWindowViewModel.cs
+++ ViewModelBaseTestApp/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -18,12 +19,22 @@ namespace ViewModelBaseTestApp
 
     class MainWindowViewModel : ViewModelBase
     {
+        private const int DEFAULT_TIME = 30;
+        private const int INTERVAL_TIME = 1000;
+        private int _countdownSecInt;
+        private Timer _timer;
         private string _itemX;
         private string _itemY;
         private string _paramZ;
         private string _output;
 
-        public MainWindowViewModel() { }
+        public MainWindowViewModel()
+        {
+            CountdownSec = DEFAULT_TIME.ToString();
+            _countdownSecInt = DEFAULT_TIME;
+            IsEditTime = true;
+            _timer = new Timer(noticeTime, null, Timeout.Infinite, 0);
+        }
 
         public string ItemX
         {
@@ -52,6 +63,68 @@ namespace ViewModelBaseTestApp
             {
                 _output = value;
                 base.RaisePropertyChanged(() => Output);
+            }
+        }
+
+        private string _countdownSec;
+        public string CountdownSec
+        {
+            get { return _countdownSec; }
+            set
+            {
+                _countdownSec = value;
+                if (int.TryParse(value, out _countdownSecInt))
+                {
+                    DisplaySec = _countdownSecInt;
+                }
+                base.RaisePropertyChanged(() => CountdownSec);
+            }
+        }
+
+        private bool _isEditTime;
+        public bool IsEditTime
+        {
+            get { return _isEditTime; }
+            set
+            {
+                _isEditTime = value;
+                base.RaisePropertyChanged(() => IsEditTime);
+                if (_isEditTime)
+                {
+                    StartButtonText = "Start";
+                }
+                else
+                {
+                    StartButtonText = "Stop";
+                }
+            }
+        }
+
+        private string _startButtonText;
+        public string StartButtonText
+        {
+            get { return _startButtonText; }
+            set
+            {
+                _startButtonText = value;
+                base.RaisePropertyChanged(() => StartButtonText);
+            }
+        }
+
+        private int _displaySec;
+        public int DisplaySec
+        {
+            get { return _displaySec; }
+            set
+            {
+                _displaySec = value;
+                base.RaisePropertyChanged(() => DisplaySec);
+
+                if (_displaySec <= 0)
+                {
+                    stopTimer();
+                    CommandManager.InvalidateRequerySuggested();
+                }
             }
         }
 
@@ -109,6 +182,70 @@ namespace ViewModelBaseTestApp
                         outputWithParamExecute, outputWithParamCanExecute);
                 return _outputWithParamCommand;
             }
+        }
+
+        private void countdownExecute()
+        {
+            if (IsEditTime)
+            {
+                IsEditTime = false;
+                _timer.Change(INTERVAL_TIME, INTERVAL_TIME);
+            }
+            else
+            {
+                stopTimer();
+            }
+        }
+        private bool countdownCanExecute(object param)
+        {
+            return DisplaySec != 0;
+        }
+        private ICommand _countdownCommand;
+        public ICommand CountdownCommand
+        {
+            get
+            {
+                if (_countdownCommand == null)
+                {
+                    _countdownCommand = new RelayCommand(countdownExecute, countdownCanExecute);
+                }
+                return _countdownCommand;
+            }
+        }
+
+        private void resetExecute()
+        {
+            DisplaySec = _countdownSecInt;
+        }
+        private ICommand _resetCommand;
+        public ICommand ResetCommand
+        {
+            get
+            {
+                if (_resetCommand == null)
+                {
+                    _resetCommand = new RelayCommand(resetExecute);
+                }
+                return _resetCommand;
+            }
+        }
+
+        private void noticeTime(object state)
+        {
+            --DisplaySec;
+        }
+
+        private void stopTimer()
+        {
+            _timer.Change(Timeout.Infinite, 0);
+            IsEditTime = true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _timer.Dispose();
+
+            base.Dispose(disposing);
         }
     }
 }
