c5ea3eb15a47b081058bdb29c23d59351230d904
MakMokMak <mak@makcraft.com>
2017-02-03 16:58:03 +0900

    マルチモニターユーティリティのテスト用アプリケーションのプロジェクトを作成 #4

---
+++ MultiMonitorUtilTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MakCraft.ViewModels;
+
+namespace MultiMonitorUtilTestApp.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
